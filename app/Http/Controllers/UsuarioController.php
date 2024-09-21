d93344afe92f37a68e981e7d441eba83eb3ea1ea
Rafa-hud <al222210482@gmail.com>
2024-09-21 14:10:09 -0600

    app/Http/Controllers/UsuarioController.php

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Usuario;
+use App\Http\Requests\StoreUsuarioRequest;
+use App\Http\Requests\UpdateUsuarioRequest;
+use Illuminate\Support\Facades\Log;
+
+class UsuarioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $a = "LLegas a la funcion";
+        Log::emergency($a);
+        Log::alert(message: $a);
+        Log::critical($a);
+        Log::error($a);
+        Log::warning($a);
+        Log::notice($a);
+        Log::info($a);
+        Log::debug($a);
+        $user = Usuario::all();
+        return view('vistas.index', compact('user'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUsuarioRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Usuario $usuario)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Usuario $usuario)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUsuarioRequest $request, Usuario $usuario)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Usuario $usuario)
+    {
+        //
+    }
+
+    public function FunctionTest() {}
+
+    public function DeveloperFunction() {}
+}
